dcf16936cdb3c935f3917e9d0935d95001afc076
shenhua889 <shenhua889@126.com>
2017-09-12 17:32:16 +0800

    2017/9/12
    主界面的建立
    数据库的搭建

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace postKCGL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.单位修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.入库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.出库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.统计ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.单位修改ToolStripMenuItem,
+            this.入库ToolStripMenuItem,
+            this.出库ToolStripMenuItem,
+            this.库存查询ToolStripMenuItem,
+            this.统计ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1136, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 单位修改ToolStripMenuItem
+            // 
+            this.单位修改ToolStripMenuItem.Name = "单位修改ToolStripMenuItem";
+            this.单位修改ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.单位修改ToolStripMenuItem.Text = "单位修改";
+            this.单位修改ToolStripMenuItem.Click += new System.EventHandler(this.单位修改ToolStripMenuItem_Click);
+            // 
+            // 入库ToolStripMenuItem
+            // 
+            this.入库ToolStripMenuItem.Name = "入库ToolStripMenuItem";
+            this.入库ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.入库ToolStripMenuItem.Text = "入库";
+            this.入库ToolStripMenuItem.Click += new System.EventHandler(this.入库ToolStripMenuItem_Click);
+            // 
+            // 出库ToolStripMenuItem
+            // 
+            this.出库ToolStripMenuItem.Name = "出库ToolStripMenuItem";
+            this.出库ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.出库ToolStripMenuItem.Text = "出库";
+            this.出库ToolStripMenuItem.Click += new System.EventHandler(this.出库ToolStripMenuItem_Click);
+            // 
+            // 库存查询ToolStripMenuItem
+            // 
+            this.库存查询ToolStripMenuItem.Name = "库存查询ToolStripMenuItem";
+            this.库存查询ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.库存查询ToolStripMenuItem.Text = "库存查询";
+            this.库存查询ToolStripMenuItem.Click += new System.EventHandler(this.库存查询ToolStripMenuItem_Click);
+            // 
+            // 统计ToolStripMenuItem
+            // 
+            this.统计ToolStripMenuItem.Name = "统计ToolStripMenuItem";
+            this.统计ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.统计ToolStripMenuItem.Text = "统计";
+            this.统计ToolStripMenuItem.Click += new System.EventHandler(this.统计ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 660);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 单位修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 入库ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 出库ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 库存查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 统计ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace postKCGL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool DecideChildForm()
+        {
+            if (this.MdiChildren.Length == 0)
+                return true;
+            {
+                MessageBox.Show("已有其他窗口打开");
+                return false;
+            }
+        }
+        private void 单位修改ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(DecideChildForm())
+            {
+                DWXG dwxg = new DWXG();
+                dwxg.MdiParent = this;
+                dwxg.WindowState = FormWindowState.Maximized;
+                dwxg.Show();
+            }
+        }
+
+        private void 入库ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(DecideChildForm())
+            {
+                RK rk = new RK();
+                rk.MdiParent = this;
+                rk.WindowState = FormWindowState.Maximized;
+                rk.Show();
+            }
+        }
+
+        private void 出库ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DecideChildForm())
+            {
+                CK ck = new CK();
+                ck.MdiParent = this;
+                ck.WindowState = FormWindowState.Maximized;
+                ck.Show();
+            }
+        }
+
+        private void 库存查询ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DecideChildForm())
+            {
+                CX cx = new CX();
+                cx.MdiParent = this;
+                cx.WindowState = FormWindowState.Maximized;
+                cx.Show();
+            }
+        }
+
+        private void 统计ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DecideChildForm())
+            {
+                TJ tj = new TJ();
+                tj.MdiParent = this;
+                tj.WindowState = FormWindowState.Maximized;
+                tj.Show();
+            }
+        }
+    }
+}
