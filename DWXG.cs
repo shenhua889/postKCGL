e71940b115f9d252d96837abd078b962cf606627
shenhua889 <shenhua889@126.com>
2017-09-15 16:58:48 +0800

    9月15日
    单位修改基本完成，下周完成入库和出库吧~~~~

--- DWXG.cs
+++ DWXG.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace postKCGL
 {
     public partial class DWXG : Form
@@ -16,10 +16,106 @@ namespace postKCGL
         {
             InitializeComponent();
         }
-
+        DataTable dt;
+        MySqlHelper msh = new MySqlHelper(Properties.Resources.MySqlConnStr);
         private void DWXG_Load(object sender, EventArgs e)
         {
+            FormLoad();
+        }
+        private void FormLoad()
+        {
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            dt = msh.GetDataTable("select * from unit");
+            foreach (DataRow dr in dt.Rows)
+            {
+                string ID = dr["ID"].ToString().PadLeft(15).PadRight(30);
+                string Name = dr["Name"].ToString();
+                listBox1.Items.Add(ID + Name);
+            }
 
+        }
+        private void ListBoxSelect(int SelectIndex)
+        {
+            listBox1.SelectedIndex = SelectIndex;
+        }
+        private void UnitFind()
+        {
+            string name = textBox2.Text;
+            int flag = 0;
+            for(int i=listBox1.SelectedIndex;i<dt.Rows.Count;i++)
+            {
+                if(listBox1.Items[i].ToString().Contains(name))
+                {
+                    ListBoxSelect(i);
+                    return;
+                }
+            }
+            if(flag==0)
+            {
+                for(int i=0;i<listBox1.SelectedIndex;i++)
+                {
+                    if (listBox1.Items[i].ToString().Contains(name))
+                    {
+                        ListBoxSelect(i);
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("没有找到该用户");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UnitFind();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string s = listBox1.SelectedItem.ToString();
+                textBox1.Text = s.Substring(0, 15).Trim();
+                textBox2.Text = s.Substring(15).Trim();
+            }
+        }
+        private void UpdateUnit()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                MySqlParameter[] msp = new MySqlParameter[2];
+                msp[0] = new MySqlParameter("?name", MySqlDbType.VarChar, 80);
+                msp[1] = new MySqlParameter("?ID", MySqlDbType.Int32, 10);
+                msp[0].Value = textBox2.Text;
+                msp[1].Value = textBox1.Text;
+                msh.ExecuteNonQuery("update Unit set name=?name where ID=?ID", msp);
+                FormLoad();
+                MessageBox.Show("修改成功");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MySqlParameter msp = new MySqlParameter("?name", MySqlDbType.VarChar, 80);
+            //msp.Value = textBox2.Text;
+            UpdateUnit();
+        }
+        private void DeleteUnit()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                MySqlParameter[] msp = new MySqlParameter[2];
+                msp[0] = new MySqlParameter("?name", MySqlDbType.VarChar, 80);
+                msp[1] = new MySqlParameter("?ID", MySqlDbType.Int32, 10);
+                msp[0].Value = textBox2.Text;
+                msp[1].Value = textBox1.Text;
+                msh.ExecuteNonQuery("delete from unit where name=?name and ID=?ID", msp);
+                FormLoad();
+                MessageBox.Show("删除成功");
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteUnit();
         }
     }
 }
---
+++ NPExcel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+namespace postKCGL
+{
+    /// <summary>
+    /// 用NPOI读取EXCEL
+    /// </summary>
+    public class NPExcel
+    {
+        private IWorkbook _WorkBook;
+        private string _FilePath;
+        public List<string> SheetNames { get; set; }
+        public NPExcel()
+        {
+            SheetNames = new List<string>();
+        }
+        public List<string> LoadFile(string FIlePath)
+        {
+            _FilePath = FIlePath;
+            using (var fs = new FileStream(FIlePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                    _WorkBook = WorkbookFactory.Create(fs);
+            }
+            return GetSheetNames();
+        }
+        private List<string> GetSheetNames()
+        {
+            SheetNames.Clear();
+            var count = _WorkBook.NumberOfSheets;
+            for(int i=0;i<count;i++)
+            {
+                SheetNames.Add(_WorkBook.GetSheetName(i));
+            }
+            return SheetNames;
+        }
+        /// <summary>
+        /// 默认为第一个SheetName
+        /// </summary>
+        /// <returns></returns>
+        public DataTable ExcelToDataTable()
+        {
+            //GetSheetNames();
+            if (SheetNames.Count != 0)
+                return ExcelToDataTable(SheetNames[0]);
+            else
+                return null;
+        }
+        public DataTable ExcelToDataTable(string SheetName)
+        {
+            try
+            {
+                ISheet sheet = _WorkBook.GetSheet(SheetName);
+                DataTable dt = new DataTable();
+                IRow FirstRow = sheet.GetRow(0);
+                //添加列
+                int CellCount = FirstRow.LastCellNum;
+                for (int i = FirstRow.FirstCellNum; i < CellCount; i++)
+                {
+                    string CellName = FirstRow.GetCell(i).StringCellValue;
+                    DataColumn dc = new DataColumn(CellName);
+                    dt.Columns.Add(dc);
+                }
+                //添加内容
+                int StartRowNum = sheet.FirstRowNum + 1;
+                int RowCount = sheet.LastRowNum;
+                for (int i = StartRowNum; i <= RowCount; i++)
+                {
+                    IRow row = sheet.GetRow(i);
+                    if (row == null) continue;
+                    DataRow dr = dt.NewRow();
+                    for (int j = FirstRow.FirstCellNum; j < CellCount; j++)
+                    {
+                        if (row.GetCell(j) != null)
+                        {
+                            dr[j] = row.GetCell(j, MissingCellPolicy.RETURN_NULL_AND_BLANK).ToString();
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+                return dt;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("错误表为:" + _FilePath + "\r\n" + "报错信息为:" + e.ToString());
+                return null;
+            }
+        }
+        /// <summary>
+        /// DataTable导出到EXCEL
+        /// </summary>
+        /// <param name="Table">表格</param>
+        /// <param name="SaveFile">保存文件地址</param>
+        /// <returns></returns>
+        public string DataTableToExcel(DataTable Table,string SaveFile)
+        {
+            //IWorkbook WorkBook = new XSSFWorkbook();
+            IWorkbook WorkBook = new HSSFWorkbook();
+            ISheet Sheet = WorkBook.CreateSheet();
+            int RowsCount = Table.Rows.Count;
+            int ColsCount = Table.Columns.Count;
+            IRow Row = Sheet.CreateRow(0);
+            ICell Cell = null;
+            for(int i=0;i<ColsCount;i++)
+            {
+                Cell = Row.CreateCell(i);
+                Cell.SetCellValue(Table.Columns[i].ColumnName);
+                Sheet.AutoSizeColumn(i);
+            }
+            for(int i=0;i<RowsCount;i++)
+            {
+                Row = Sheet.CreateRow(i + 1);
+                for(int j=0;j<ColsCount;j++)
+                {
+                    Cell = Row.CreateCell(j);
+                    Cell.SetCellValue(Table.Rows[i][j].ToString());
+                }
+            }
+            //表格宽度
+            for(int i=0;i<ColsCount;i++)
+            {
+                int ColLenght = 0;
+                for(int j=0;j<=Sheet.LastRowNum;j++)
+                {
+                    int CellLenght =Encoding.Default.GetBytes(Sheet.GetRow(j).GetCell(i).StringCellValue).Length;
+                    if (CellLenght > ColLenght)
+                        ColLenght = CellLenght;
+                }
+                Sheet.SetColumnWidth(i, ColLenght * 256);
+            }
+            using (FileStream fs = File.OpenWrite(SaveFile))
+            {
+                WorkBook.Write(fs);
+                return "保存成功";
+                
+            }
+        }
+    }
+}
