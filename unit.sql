9462aa2e70b0f643c80d55d29d3d2a08e82cf62e
shenhua889 <shenhua889@126.com>
2017-09-30 17:36:12 +0800

    9月30日

---
+++ unit.sql
@@ -0,0 +1,7 @@
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (1, '沈骅', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (2, '陈立顺', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (6, '邮政', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (7, '广州', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (8, '杭州', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (9, '南京', 0, 0);
+INSERT INTO `unit` (`ID`, `Name`, `RC`, `Flag`) VALUES (10, '沈阳', 0, 0);
